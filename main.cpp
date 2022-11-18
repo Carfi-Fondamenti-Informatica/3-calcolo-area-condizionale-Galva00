edb423b9295782547f59633e10480fcb659e9db6
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-11-18 23:24:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+    int opzione=0;
+    float a=0, b=0;
+    cin >> a >> b >> opzione;
+    if (-1<opzione and opzione<3) {
+        switch (opzione) {
+            case 0: cout << (a*b)/2 <<endl;
+            break;
+            case 1: cout << a*a <<endl;
+            break;
+            case 2: cout << a*b <<endl;
+            break;
+        }
+    } else  {
+        cout <<  "opzione non valida" <<endl;
+    }
    return 0;
 }
